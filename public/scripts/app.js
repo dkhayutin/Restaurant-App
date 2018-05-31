3ed5b969afcd1a54177d3c08b3bb59fd779deb70
dkhayutin <dan.khayutin@gmail.com>
2018-05-31 20:12:37 +0000

    added flip and styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,19 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
+});
 
 
 $('document').ready(function(){
-  console.log('meep')
+  var count = 0;
  $("img").click(function(){
-  alert("it works!");
+  count ++;
+  alert("counter: " + count);
  });
 });
