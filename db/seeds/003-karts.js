f13e08b40d08e597bcecdda66788d3a4dbbd25d3
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-05-31 19:39:21 +0000

    Keep the database simple

--- db/seeds/003-karts.js
+++ db/seeds/003-karts.js
@@ -2,11 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('karts').del()
     .then(function () {
       return Promise.all([
-        knex('karts').insert({id: 1, users_id: '1', karts_id: '1', dishes_id: 1, quantity: 1}),
-        knex('karts').insert({id: 2, users_id: '2', karts_id: '2', dishes_id: 1, quantity: 1}),
-        knex('karts').insert({id: 3, users_id: '1', karts_id: '1', dishes_id: 3, quantity: 1}),
-        knex('karts').insert({id: 4, users_id: '2', karts_id: '2', dishes_id: 3, quantity: 1}),
-        knex('karts').insert({id: 5, users_id: '3', karts_id: '3', dishes_id: 1, quantity: 1})
+        knex('karts').insert({id: 1, users_id: '1', dishes_id: 1, quantity: 1}),
+        knex('karts').insert({id: 2, users_id: '2', dishes_id: 2, quantity: 1}),
+        knex('karts').insert({id: 3, users_id: '1', dishes_id: 3, quantity: 1}),
+        knex('karts').insert({id: 4, users_id: '2', dishes_id: 3, quantity: 1}),
+        knex('karts').insert({id: 5, users_id: '3', dishes_id: 1, quantity: 1})
       ]);
     });
 };
