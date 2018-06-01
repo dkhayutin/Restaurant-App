b075f7f538311dff75fb9fe25d1e4beea8628e98
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-01 18:24:02 +0000

    post kart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,39 @@
 $(() => {
+  // loadALL();
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((dishes) => {
+  //   for(user of dishes) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+});
+
+function loadALL(restaurant_id) {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/api/dishes"
+  }).done((dishes) => {
+    for (let i = 0; i < dishes.length; i++) {
+      console.log(dishes[i].name);
+      $("<div>").text(dishes[i].name).appendTo($("body"));
     }
-  });;
+  });
+}
+
+
+$('document').ready(function(){
+
+ $(".order").click(function(){
+  let itemId = $(this).data('id');
+  //AJAX post
+   // $.post(`/kart`, {itemId: itemId});
+   $.post('/kart');
+  });
 });
+
+//TODO
+// 1. Move counter of items to db
+// 2. Get prices from seed and sum float value (decimals)
+// 3. Link randomNumber to
